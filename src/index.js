0cc7427bfcbaac96c4c67f9d918bb828d53e0413
Cristian Incarnato <cristian.cdi@gmail.com>
2022-02-28 17:04:35 -0300

    Run default on port 80

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use('/api',DockerContainerRoutes)
 app.use(ErrorHandlerMiddleware)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 80
 
 app.listen(PORT, () => {
     console.log("Started app on port: " + PORT)
