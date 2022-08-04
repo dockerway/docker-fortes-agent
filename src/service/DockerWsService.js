93a8d9743f6069eed6b1cd9f18b854f547af25eb
Cristian Incarnato <cristian.cdi@gmail.com>
2022-08-04 09:15:40 -0300

    2 consolas funcionando mismo Container

--- src/service/DockerWsService.js
+++ src/service/DockerWsService.js
@@ -6,32 +6,32 @@ const startWebSocketServerWithDocker = () => {
     wsServer.on('connection', (ws) => {
         console.log("OnConnection clients size:", wsServer.clients.size)
         ws.on('message', async (data) => {
-            let json = JSON.parse(data.toString()) // {containerId: xx, payload: yy }
+            let json = JSON.parse(data.toString()) // {wsId: zz, containerId: xx, payload: yy }
 
             if (!ws.dockerStream) {
                 ws.dockerStream = {}
             }
 
             //INICIALIZO EL DOCKER STREAM SI NO EXISTE, UNO POR CADA CONTENEDOR DIFERENTE
-            if (!ws.dockerStream[json.containerId]) {
+            if (!ws.dockerStream[json.wsId]) {
                 //OBTENGO EL STREAM
-                ws.dockerStream[json.containerId] = await getStreamFromContainerExec(json.containerId, 'bash')
+                ws.dockerStream[json.wsId] = await getStreamFromContainerExec(json.containerId, 'bash')
                 //Si no obtube el stream genero error y retorno
-                if(!ws.dockerStream[json.containerId]){
+                if(!ws.dockerStream[json.wsId]){
                     console.error("DockerStream doesnt work")
                     return
                 }
                 //AL RECIBIR DATOS DEL CONTENEDOR LOS MANDO AL WS
-                ws.dockerStream[json.containerId].on('data', (chunk) => {
-                    const terminalMessage = {containerId: json.containerId, payload: chunk.toString()}
+                ws.dockerStream[json.wsId].on('data', (chunk) => {
+                    const terminalMessage = {wsId: json.wsId, containerId: json.containerId, payload: chunk.toString()}
                     console.log('WS SEND',terminalMessage)
                     ws.send(JSON.stringify(terminalMessage))
                 })
             }
             //ESCRIBO LOS DATOS RECIBIDOS POR EL WS EN EL DOCKER STREAM
-            if(ws.dockerStream[json.containerId]){
+            if(ws.dockerStream[json.wsId]){
                 console.log('DockerStream write',json.payload);
-                ws.dockerStream[json.containerId].write(json.payload);
+                ws.dockerStream[json.wsId].write(json.payload);
             }
 
 
