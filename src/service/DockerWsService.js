3cbde2f77d6c78ed045e8579643fcb205fe9e59d
ramiro.sandoval <ramiro.sandoval@sondeos.com.ar>
2023-01-04 12:26:36 -0300

    little advancement in #59792

--- src/service/DockerWsService.js
+++ src/service/DockerWsService.js
@@ -16,21 +16,73 @@ const startWebSocketServerWithDocker = () => {
             if (!ws.dockerStream[json.wsId]) {
                 //OBTENGO EL STREAM
                 ws.dockerStream[json.wsId] = await getStreamFromContainerExec(json.containerId, json.terminalSelected)
-                //Si no obtube el stream genero error y retorno
-                if(!ws.dockerStream[json.wsId]){
+                //Si no obtuve el stream genero error y retorno
+                if (!ws.dockerStream[json.wsId]) {
                     console.error("DockerStream doesnt work")
                     return
                 }
                 //AL RECIBIR DATOS DEL CONTENEDOR LOS MANDO AL WS
                 ws.dockerStream[json.wsId].on('data', (chunk) => {
-                    const terminalMessage = {wsId: json.wsId, containerId: json.containerId, payload: chunk.toString()}
-                    //console.log('WS SEND',terminalMessage)
+                    console.log("CHUNK: ", chunk, chunk.toString())
+
+                    function deleteDockerHeaders(chunk) {
+
+                        let headersIndex = chunk.indexOf('01000000', 0, 'hex')
+                        let initialHeadersHadBeenDeleted = false
+                        let chunkHasNoHeaders = false
+                        let trimmeredChunkArray = []
+
+                        while (!chunkHasNoHeaders) { //chequear headers -> guardar todo lo precedente asi como lo procedente a ellos -> goto 1
+                            if (headersIndex == -1) {
+                                chunkHasNoHeaders = true
+                                break
+                            }
+                            
+                            console.log(`Inside While loop | headersindex: '${headersIndex}'`)
+
+                            if (initialHeadersHadBeenDeleted) { // se eliminaron los headers del inicio
+                                console.log('initialHeadersHadBeenDeleted')
+                                for (let index = 0; index < headersIndex; index++) {
+                                    trimmeredChunkArray.push(chunk[index])
+                                    console.log(trimmeredChunkArray)
+                                }
+
+                                chunk = chunk.subarray(headersIndex + 8)
+                                headersIndex = chunk.indexOf('01000000', 0, 'hex')
+                            } else {
+                                console.log('else')
+                                chunk = chunk.subarray(headersIndex + 8)
+                                initialHeadersHadBeenDeleted = true
+                            }
+
+                        }
+
+                        console.log(trimmeredChunkArray)
+                        const trimmeredChunk = Buffer.from(trimmeredChunkArray)
+                        console.log(trimmeredChunk)
+
+                        return trimmeredChunk
+                    }
+
+                    function showBuffer(chunk) {
+                        for (let index = 0; index < chunk.length; index++) {
+                            console.log(chunk[index])
+                        }
+                    }
+
+                    showBuffer(chunk)
+
+                    chunk = deleteDockerHeaders(chunk)
+                    console.log("CHUNK with no headers: ", chunk, chunk.toString())
+
+                    const terminalMessage = { wsId: json.wsId, containerId: json.containerId, payload: chunk.toString() }
+                    // console.log('WS SEND',terminalMessage)
                     ws.send(JSON.stringify(terminalMessage))
                 })
             }
             //ESCRIBO LOS DATOS RECIBIDOS POR EL WS EN EL DOCKER STREAM
-            if(ws.dockerStream[json.wsId]){
-                //console.log('DockerStream write',json.payload);
+            if (ws.dockerStream[json.wsId]) {
+                console.log('DockerStream write', json.payload);
                 ws.dockerStream[json.wsId].write(json.payload);
             }
 
@@ -47,6 +99,7 @@ const getStreamFromContainerExec = (containerID, terminal = 'bash') => {
 
         function handle(error) {
             console.error("HANDLE ERROR", error);
+            reject(error)
         }
 
         const executionParameters = {
@@ -61,7 +114,7 @@ const getStreamFromContainerExec = (containerID, terminal = 'bash') => {
         selectedContainer.exec(executionParameters, (error, exec) => {
             if (error) handle(error);
 
-            exec.start({stdin: true, stdout: true, stderr: true}, (error, stream) => {
+            exec.start({ stdin: true, stdout: true, stderr: true }, (error, stream) => {
                 if (error) handle(error)
 
                 resolve(stream)
