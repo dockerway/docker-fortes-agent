c2b42f4a90fee0256c2c5c4c72b287369d6d2cac
ramiro.sandoval <ramiro_sando@outlook.com>
2023-04-24 17:55:16 -0300

    #78541

--- src/routes/DockerContainerRoutes.js
+++ src/routes/DockerContainerRoutes.js
@@ -6,31 +6,38 @@ const router = express.Router();
 
 function validateStatusCode(statusCode){
     return http.STATUS_CODES.hasOwnProperty(statusCode)
-};
+}
 
 router.get('/docker/container/:containerid/stats', async function (req, res) {
     try {
-        const response = await containerStats(req.params.containerid);
-        res.json(response);
+        const response = await containerStats(req.params.containerid)
+        res.json(response)
     } catch (error) {
-        const statusCode = (error.statusCode && validateStatusCode(error.statusCode)) ? error.statusCode : 500;
-        res.status(statusCode);
-        res.send(error.message);
-    }
-});
-
-
-router.post('/docker/folders', async function (req, res){
-    try {
-        if(!Array.isArray(req.body)) throw new Error("Request body must be an Array!")
-        const response = await foldersCreator(req.body)
-        res.status(200)
-        res.json({response: response})
-    } catch(error){
         const statusCode = (error.statusCode && validateStatusCode(error.statusCode)) ? error.statusCode : 500
+
         res.status(statusCode)
         res.send(error.message)
     }
-});
+})
+
+
+router.post('/docker/folders', function (req, res){
+    try {
+        if (!Array.isArray(req.body)) throw new Error("Request body must be an Array!")
+        const response = foldersCreator(req.body)
+
+        console.log(`request body: ${JSON.stringify(req.body)}`)
+        console.log(`response from folders endpoint: ${JSON.stringify(response)}`)
+
+        res.status(200)
+        res.json({response: response})
+
+    } catch(error){
+        const statusCode = (error.statusCode && validateStatusCode(error.statusCode)) ? error.statusCode : 500
+
+        res.status(statusCode)
+        res.send(error.message)
+    }
+})
 
 module.exports = router;
