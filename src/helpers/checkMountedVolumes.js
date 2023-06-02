903264f3e0d6795078dfd8babf64f80f690377c1
ramiro.sandoval <ramiro_sando@outlook.com>
2023-06-02 12:42:55 -0300

    feat: Update logic to return false if any directory doesn't exist
    - Modify the code to check if any directory doesn't exist
    - Replace forEach loop with a for loop for early termination
    
    This commit updates the logic in the checkIfMountedDirectoriesExists function to return false if any of the directories specified in the settings are not found. The code now uses fs.existsSync to check the existence of each directory and terminates the loop if a directory doesn't exist. This ensures that the result is false when any directory is missing.
    
    Closes #85713

--- src/helpers/checkMountedVolumes.js
+++ src/helpers/checkMountedVolumes.js
@@ -1,26 +1,26 @@
 const fs = require('fs');
 
 function checkIfMountedDirectoriesExists() {
+  const neededVolumes = process.env.NEEDEDVOLUMES ? process.env.NEEDEDVOLUMES : '/localdata,/logs,/storage,/var'
+  const directories = neededVolumes.split(',').filter(element => element.startsWith('/'))
 
-    const neededVolumes = process.env.NEEDEDVOLUMES ? process.env.NEEDEDVOLUMES : '/localdata,/logs,/storage,/var'
-    const directories = neededVolumes.split(',').filter(element => element.startsWith('/'));
-
-    let result = null
-
-    try {
-        directories.forEach(directory => {
-            result = fs.existsSync(directory)
-            console.log(`checkIfMountedDirectoriesExists result: '${result}'. Last directory checked: '${directory}'`)
-        })
-
-        return result
-    } catch (error) {
-        console.error(`An error happened at checkIfMountedDirectoriesExists: '${error}'`)
-        throw error
+  try {
+    for (let i = 0; i < directories.length; i++) {
+      if (!fs.existsSync(directories[i])) {
+        console.log(`Directory does not exist: '${directories[i]}'`)
+        return false
+      }
     }
+
+    console.log('All directories exist.')
+    return true
+  } catch (error) {
+    console.error(`An error occurred at checkIfMountedDirectoriesExists: '${error}'`)
+    throw error
+  }
 }
 
 const notMountedMessage = 'The needed directories are not mounted; please contact your infrastructure team!'
 
-module.exports.checkIfMountedDirectoriesExists = checkIfMountedDirectoriesExists
-module.exports.notMountedMessage = notMountedMessage
+module.exports.checkIfMountedDirectoriesExists = checkIfMountedDirectoriesExists;
+module.exports.notMountedMessage = notMountedMessage;
