33c5a049f2c6e80077a5382056518485f449893a
ramiro.sandoval <ramiro_sando@outlook.com>
2023-08-01 15:38:57 -0300

    (fix): containerStats function now checks if metric const exists before using it (plus general refactor)

--- src/service/ContainerService.js
+++ src/service/ContainerService.js
@@ -1,69 +1,67 @@
 const Docker = require('dockerode');
-const docker = new Docker({socketPath: '/var/run/docker.sock'});
+const docker = new Docker({ socketPath: '/var/run/docker.sock' });
 
 const createDirIfDoesntExist = require('../helpers/createDirIfDoesntExist');
 const { checkIfMountedDirectoriesExists, notMountedMessage } = require('../helpers/checkMountedVolumes');
 
 function formatBytes(bytes, decimals = 2) {
-    if (bytes === 0) return '0 Bytes'
+    try {
+        if (bytes === 0) return '0 Bytes'
 
-    const k = 1024
-    const dm = decimals < 0 ? 0 : decimals
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+        const k = 1024
+        const dm = decimals < 0 ? 0 : decimals
+        const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
 
-    const i = Math.floor(Math.log(bytes) / Math.log(k))
+        const i = Math.floor(Math.log(bytes) / Math.log(k))
 
-    return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
+        return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i]
+    } catch (error) {
+        console.error(`An error happened at the 'formatBytes' function: '${error.message ? error.message : error}'`)
+        throw error
+    }
 }
 
-function containerStats(containerId) {
+async function containerStats(containerId) {
     try {
-        const container = docker.getContainer(containerId)
+        const metric = await docker.getContainer(containerId).stats({ stream: false })
 
-        const opts =  {stream: false}
-        const metric = container.stats(opts)
+        if (metric) {
+            const cpuDelta = metric.cpu_stats.cpu_usage.total_usage - metric.precpu_stats.cpu_usage.total_usage
+            const systemDelta = metric.cpu_stats.system_cpu_usage - metric.precpu_stats.system_cpu_usage
+            const cpu = cpuDelta / systemDelta * 100
 
-        const stats = {
-            cpu: '',
-            memoryLimit: '',
-            memoryUsage: ''
+            const stats = {
+                cpu: Math.round((cpu + Number.EPSILON) * 100) / 100,
+                memoryLimit: formatBytes(metric.memory_stats.limit),
+                memoryUsage: formatBytes(metric.memory_stats.usage)
+            }
+            
+            console.log(`containerStats debug: | stats: '${JSON.stringify(stats, null, 2)}`)
+            return stats
         }
-
-        //CPU
-        let cpuDelta = metric.cpu_stats.cpu_usage.total_usage -  metric.precpu_stats.cpu_usage.total_usage
-        let systemDelta = metric.cpu_stats.system_cpu_usage - metric.precpu_stats.system_cpu_usage
-        // let cpu= cpuDelta / systemDelta * metric.cpu_stats.cpu_usage.percpu_usage.length * 100
-        let cpu= cpuDelta / systemDelta * 100
-        stats.cpu = Math.round((cpu + Number.EPSILON) * 100) / 100
-
-
-        //Memory
-        stats.memoryLimit = formatBytes(metric.memory_stats.limit)
-        stats.memoryUsage = formatBytes(metric.memory_stats.usage)
-
-        console.log("stats ", stats)
-        return stats
     } catch (error) {
+        console.error(`An error happened at the 'containerStats' function: '${error.message ? error.message : error}'`)
         throw error
     }
 }
 
 function foldersCreator(volumes) {
-    if (checkIfMountedDirectoriesExists()){
+    if (checkIfMountedDirectoriesExists()) {
         try {
             let created = false
             const volumesCreated = []
 
-            for(let i = 0; i < volumes.length; i++){
+            for (let i = 0; i < volumes.length; i++) {
                 created = createDirIfDoesntExist(volumes[i]) //create the directory  
                 created ? volumesCreated.push({ volume: volumes[i], created: created }) : volumesCreated
             }
 
             return volumesCreated
         } catch (error) {
+            console.error(`An error happened at the 'foldersCreator' function: '${error.message ? error.message : error}'`)
             throw error
         }
-    }else{
+    } else {
         return notMountedMessage
     }
 }
