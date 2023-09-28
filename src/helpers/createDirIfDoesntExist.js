006dd46245e739528377bdbd8e1932d86450a59b
ramiro.sandoval <ramiro_sando@outlook.com>
2023-09-28 18:20:06 -0300

    (fix): permissions are now set correctly

--- src/helpers/createDirIfDoesntExist.js
+++ src/helpers/createDirIfDoesntExist.js
@@ -2,8 +2,12 @@ const fs = require('fs');
 
 const createDirIfDoesntExist = function(directoryPath){
     try {
+        let oldUmask = process.umask(0) //sets the umask to zero and returns the previous one
+        
         if (!fs.existsSync(directoryPath)){
             fs.mkdirSync(directoryPath, {recursive:true})
+            oldUmask = process.umask(oldUmask)
+
             return true
         }
 
